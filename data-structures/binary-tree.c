6c83228757780edfc6388dbd6eb06eb7ef348847
Andrey Arthur <andrey_arthur1@outlook.com>
2023-11-27 09:38:13 -0300

    implement binary search tree

---
+++ data-structures/binary-tree.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  int value;
+
+  struct Node *left;
+  struct Node *right;
+};
+
+struct BinaryTree {
+  struct Node *head;
+  void(*insert)(struct BinaryTree *tree, int value);
+  void(*delete)(struct BinaryTree *tree, int value);
+  struct Node *(*search)(struct BinaryTree *tree, int value);
+  int *(*traversal)(struct BinaryTree *tree);
+  int(*size)(struct BinaryTree *tree);
+  int(*height)(struct BinaryTree *tree);
+};
+
+struct Node *_create_node(int value) {
+  struct Node *node = malloc(sizeof(struct Node));
+  node->value = value;
+  node->left = NULL;
+  node->right = NULL;
+  return node;
+}
+
+void _binary_tree_insert(struct BinaryTree *tree, int value) {
+  if (tree->head == NULL) {
+    tree->head = _create_node(value);
+    return;
+  }
+  struct Node *node = tree->head;
+  while (1) {
+    if (value <= node->value) {
+      if (node->left == NULL) {
+        node->left = _create_node(value);
+        return;
+      }
+      node = node->left;
+    } else {
+      if (node->right == NULL) {
+        node->right = _create_node(value);
+        return;
+      }
+      node = node->right;
+    }
+  }
+}
+
+void _binary_tree_delete_node(struct Node *node, struct Node *previous) {
+  if (node->value <= previous->value) {
+    if (node->left == NULL && node->right == NULL) {
+      previous->left = NULL;
+      free(node);
+      node = NULL;
+      return;
+    }
+    if (node->right == NULL) {
+      previous->left = node->left;
+      free(node);
+      node = NULL;
+      return;
+    }
+    if (node->left == NULL) {
+      previous->left = node->right;
+      free(node);
+      node = NULL;
+      return;
+    }
+    previous->left = node->right;
+    struct Node *left_node = node->left;
+    struct Node *right_node = node->right;
+    while (right_node->left != NULL) {
+      right_node = right_node->left;
+    }
+    right_node->left = left_node;
+    free(node);
+    node = NULL;
+  } else {
+    if (node->left == NULL && node->right == NULL) {
+      previous->right = NULL;
+      free(node);
+      node = NULL;
+      return;
+    }
+    if (node->right == NULL) {
+      previous->right = node->left;
+      free(node);
+      node = NULL;
+      return;
+    }
+    if (node->left == NULL) {
+      previous->right = node->right;
+      free(node);
+      node = NULL;
+      return;
+    }
+    previous->right = node->left;
+    struct Node *right_node = node->right;
+    struct Node *left_node = node->left;
+    while (left_node->right != NULL) {
+      left_node = left_node->right;
+    }
+    left_node->right = right_node;
+    free(node);
+    node = NULL;
+  }
+}
+
+void _binary_tree_delete(struct BinaryTree *tree, int value) {
+  if (tree->head == NULL) {
+    return;
+  }
+  struct Node *previous = NULL;
+  struct Node *node = tree->head;
+  while (1) {
+    if (node == NULL) {
+      return;
+    }
+    if (node->value == value) {
+      _binary_tree_delete_node(node, previous);
+      return;
+    }
+    previous = node;
+    if (value <= node->value) {
+      node = node->left;
+    } else {
+      node = node->right;
+    }
+  }
+}
+
+struct Node *_binary_tree_search(struct BinaryTree *tree, int value) {
+  struct Node *node = tree->head;
+  while (1) {
+    if (node == NULL) {
+      return NULL;
+    }
+    if (value == node->value) {
+      return node;
+    }
+    if (value < node->value) {
+      node = node->left;
+    } else {
+      node = node->right;
+    }
+  }
+}
+
+void _binary_tree_traversal_recursion(
+  struct Node *node, int *array, int *current
+) {
+  if (node) {
+    _binary_tree_traversal_recursion(node->left, array, current);
+    array[*current] = node->value;
+    (*current)++;
+    _binary_tree_traversal_recursion(node->right, array, current);
+  }
+}
+
+int *_binary_tree_traversal(struct BinaryTree *tree) {
+  int size = tree->size(tree);
+  int *array = malloc(sizeof(int) * size);
+  int current = 0;
+  _binary_tree_traversal_recursion(tree->head, array, &current);
+  return array;
+}
+
+void _binary_tree_size_recursion(struct Node *node, int *size) {
+  if (node) {
+    _binary_tree_size_recursion(node->left, size);
+    (*size)++;
+    _binary_tree_size_recursion(node->right, size);
+  }
+}
+
+int _binary_tree_size(struct BinaryTree *tree) {
+  int size = 0;
+  _binary_tree_size_recursion(tree->head, &size);
+  return size;
+}
+
+int _binary_tree_height_recursion(struct Node *node) {
+  if (node == NULL) {
+    return 0;
+  }
+  int left_height = _binary_tree_height_recursion(node->left);
+  int right_height = _binary_tree_height_recursion(node->right);
+  return (left_height > right_height) ? left_height + 1 : right_height + 1;
+}
+
+int _binary_tree_height(struct BinaryTree *tree) {
+  return _binary_tree_height_recursion(tree->head);
+}
+
+struct BinaryTree *create_binary_tree() {
+  struct BinaryTree *tree = malloc(sizeof(struct BinaryTree));
+  tree->insert = &_binary_tree_insert;
+  tree->delete = &_binary_tree_delete;
+  tree->search = &_binary_tree_search;
+  tree->traversal = &_binary_tree_traversal;
+  tree->size = &_binary_tree_size;
+  tree->height = &_binary_tree_height;
+  return tree;
+}
+
+
